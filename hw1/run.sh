63cc87d424a8c12ba8b6d15dc0a08f39ad9b8143
adypd97 <adypd1997@gmail.com>
2022-09-29 12:02:51 +0530

    added run.sh

---
+++ hw1/run.sh
@@ -0,0 +1,1 @@
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python PYTHONPATH=/Users/adypd/code/fun/learning/RL/RL_CS285/hw1 python cs285/scripts/run_hw1.py --expert_policy_file cs285/policies/experts/Ant.pkl --env_name Ant-v2 --exp_name bc_ant --n_iter 1 --expert_data cs285/expert_data/expert_data_Ant-v2.pkl --video_log_freq -1
